eda0b3b94ae84eaeecae42d6d103a197effcaf92
nikithanaga <122254357+nikithanaga@users.noreply.github.com>
2024-03-02 09:44:28 +0530

    Delete R exp directory

--- R exp/4th.R
+++
@@ -1,8 +0,0 @@
-# Set seed for reproducibility (optional)
-set.seed(123)
-
-# Create a vector of 10 random integer values between -50 and +50
-random_vector <- sample(-50:50, 10, replace = TRUE)
-
-# Print the vector
-print(random_vector)
